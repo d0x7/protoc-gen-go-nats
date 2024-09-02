dc254206641a1f7a04cb1274e5ff7e3e0ccc47e1
Dorian Heinrichs <dorian.heinrichs@xiamli.com>
2024-09-02 06:32:07 +0200

    Added docs to handlers

--- handler.go
+++ handler.go
@@ -2,14 +2,23 @@ package go_nats
 
 import "github.com/nats-io/nats.go/micro"
 
+// Those handler interfaces can be implemented by the respective NATS server impl,
+//as an alternative to setting a handler via an option when creating the server.
+
+// StatsHandler is an interface that when implemented on the server, will
+// be used directly instead of having to use the WithStatsHandler option.
 type StatsHandler interface {
 	Stats(endpoint *micro.Endpoint) any
 }
 
+// DoneHandler is an interface that when implemented on the server, will
+// be used directly instead of having to use the WithDoneHandler option.
 type DoneHandler interface {
 	Done(service micro.Service)
 }
 
+// ErrHandler is an interface that when implemented on the server, will
+// be used directly instead of having to use the WithErrorHandler option.
 type ErrHandler interface {
 	Err(service micro.Service, natsErr *micro.NATSError)
 }
